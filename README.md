5c27e71c00f1d729484afd6b8366b069c98246a4
gerardo <gerardo8a@gmail.com>
2018-04-24 22:21:00 +0200

    Introduce minor improvements
    -Introduce a constants.py file for url and csv
    -Modularize a bit the code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # portfolio_tracker
 
 A small portfolio utility that allows to track a financial portfolio.
+
+Create a constants.py file that includes URL and CSV.
+URL = URL to request between ""
+CSV = name of csv file between ""
--- tracker.py
+++ tracker.py
@@ -1,17 +1,30 @@
+from constants import *
 import csv
 import urllib.request
 import json
 
+def send_get_request(url):
+    response = urllib.request.urlopen(url)
+    json_back = response.read()
+    json_back = json.loads(json_back.decode('utf-8'))
+    #json_pretty_print(json_back)
+    return json_back
+
+def json_pretty_print(parsed):
+    print(json.dumps(parsed, indent=4, sort_keys=True))
+
 def main():
 
-    #Read csv file
-    with open('portfolio.csv', 'rt') as portfolio:
-        reader=csv.reader(portfolio)
-        
-        #Make API call
-        with urllib.request.urlopen(url) as response:
-            data = response.read()
-            print(json.loads(data.decode('utf-8')))
+    #The request returns a json that contains the info for
+    #all the tickers in exchange.
+    json_back = send_get_request(URL)
+
+    with open(CSV, 'rt') as f:
+        reader=csv.reader(f)
+
+        for row in reader:
+            ticker = row[0]
+            quantity = row[1]
 
 if __name__ == "__main__":
     main()
